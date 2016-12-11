b36e03f824d56f6b17f6e310caf2984fc654a79c
Chingyu Mo <maoqyhz@outlook.com>
2016-12-11 18:16:38 +0800

    添加注释

--- Gradient Descent/gradient_descent.py
+++ Gradient Descent/gradient_descent.py
@@ -2,11 +2,21 @@
 
 #随机梯度
 def stochastic_gradient_descent(x,y,theta,alpha,m,max_iter):
+	"""随机梯度下降法，每一次梯度下降只使用一个样本。
+
+	:param x: 训练集种的自变量
+	:param y: 训练集种的因变量
+	:param theta: 待求的权值
+	:param alpha: 学习速率
+	:param m: 样本总数
+	:param max_iter: 最大迭代次数
+
+	"""
 	deviation = 1
 	iter = 0	
 	flag = 0
 	while True:
-		for i in range(m):	
+		for i in range(m):	#循环取训练集中的一个
 			deviation = 0
 			h = theta[0] * x[i][0] + theta[1] * x[i][1]
 			theta[0] = theta[0] + alpha * (y[i] - h)*x[i][0] 
@@ -25,13 +35,23 @@ def stochastic_gradient_descent(x,y,theta,alpha,m,max_iter):
 
 #批量梯度
 def batch_gradient_descent(x,y,theta,alpha,m,max_iter):
+	"""批量梯度下降法，每一次梯度下降使用训练集中的所有样本来计算误差。
+
+	:param x: 训练集种的自变量
+	:param y: 训练集种的因变量
+	:param theta: 待求的权值
+	:param alpha: 学习速率
+	:param m: 样本总数
+	:param max_iter: 最大迭代次数
+
+	"""
 	deviation = 1
 	iter = 0
 	while deviation > EPS and iter < max_iter:
 		deviation = 0
 		sigma1 = 0
 		sigma2 = 0
-		for i in range(m):
+		for i in range(m): #对训练集中的所有数据求和迭代
 			h = theta[0] * x[i][0] + theta[1] * x[i][1]
 			sigma1 = sigma1 +  (y[i] - h)*x[i][0] 
 			sigma2 = sigma2 +  (y[i] - h)*x[i][1] 
@@ -44,11 +64,12 @@ def batch_gradient_descent(x,y,theta,alpha,m,max_iter):
 	return theta, iter
 
 
-
+#运行 为两种算法设置不同的参数
 # data and init 
 matrix_x = [[2.1,1.5],[2.5,2.3],[3.3,3.9],[3.9,5.1],[2.7,2.7]]
 matrix_y = [2.5,3.9,6.7,8.8,4.6]
 
+#随机梯度
 theta = [2,-1]
 ALPHA = 0.05
 MAX_ITER = 2000
@@ -58,6 +79,7 @@ resultTheta,iters = stochastic_gradient_descent(matrix_x, matrix_y, theta, ALPHA
 print 'theta=',resultTheta
 print 'iters=',iters
 
+#批量梯度
 theta = [2,-1]
 ALPHA = 0.05
 MAX_ITER = 1000
